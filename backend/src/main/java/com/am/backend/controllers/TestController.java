5ff071bfede2dc9b91b460b78689d4ac51cb0676
ansis maleckis <maleckisnansis@gmail.com>
2022-12-31 14:44:53 +0200

    izdevas sapakot un palaist ka vienu .jar failu

--- backend/src/main/java/com/am/backend/controllers/TestController.java
+++ backend/src/main/java/com/am/backend/controllers/TestController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:8080", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:8081", maxAge = 3600)
 @RestController
 public class TestController {
     @GetMapping("/test")
     public TestModel test(){
+        System.out.println("~~~~~~~~~~~request from the frontend~~~~~~~~~~~\n");
         return new TestModel();
     }
 }
